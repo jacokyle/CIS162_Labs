432a7007abcb314ac0cce812e1e283c8717dd779
Kyle Jacobson <54565291+jacokyle@users.noreply.github.com>
2020-04-14 23:47:47 -0400

    ASCII Art

---
+++ Lab 1/Art.java
@@ -0,0 +1,23 @@
+/**
+ * Write a description of class Art here.
+ *
+ * Kyle Jacobson
+ * Brendan Stovell
+ * version 1.0
+ */
+
+public class Art
+{
+public static void main(String [] args){
+ System.out.println("     _______  _  ");
+ System.out.println("    /^^^^^^^\\| | ");
+ System.out.println("   /^^^^^^^^^\\ | ");
+ System.out.println("  /^_^_^_^_^_^\\| ");
+ System.out.println("  |           || ");
+ System.out.println("  |  |=|  |=| || ");
+ System.out.println("  |           || ");
+ System.out.println("  |    .__.   || ");
+ System.out.println("  |    | .|   || ");
+ System.out.println("  |____|__|___|| ");
+     }
+ }
